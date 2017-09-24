651ab5524803c263cae9c2b6ade825628cb5d8ef
timhwang21 <timhwang21@gmail.com>
2017-09-24 13:19:16 -0400

    CSS width adjustments

--- src/components/layout/Row.js
+++ src/components/layout/Row.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import P from 'prop-types';
+import C from 'classnames';
 
 import { Hidable } from '../decorator';
 
 import './Row.css';
 
-const Row = ({ children }) => (
-  <div className="Row">
+const Row = ({ children, halfWidth }) => (
+  <div className={C('Row', halfWidth ? 'half-width' : 'full-width')}>
     {children}
   </div>
 );
 
 Row.propTypes = {
   children: P.node,
+  halfWidth: P.bool,
 };
 
 export default Hidable(Row);
